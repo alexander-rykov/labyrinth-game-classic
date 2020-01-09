fd78375b03c6411574304e6403b96f775a3757b8
Aliaksandr Rykau <19323417+alexander-rykov@users.noreply.github.com>
2020-01-09 15:42:39 +0000

    Add a special greeting.

--- LabyrinthApp/Program.cs
+++ LabyrinthApp/Program.cs
@@ -10,7 +10,7 @@ namespace LabyrinthApp
     {
         static void Main(string[] args)
         {
-          Console.WriteLine("Hello, world!");
+          Console.WriteLine("Hello, world! Hi Alex!");
         }
     }
 }
